b314247fc9ab8610b90a22066d4cb7c7575f0bc3
Wanwamy <longvu1217@gmail.com>
2023-10-18 13:12:04 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          >
+            test react with nodejs + ...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
